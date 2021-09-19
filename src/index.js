eb3aeedd867a1bf5571c3afaac37019cce525667
As'ad Saleh Umar <asad@infradigital.io>
2021-09-19 07:49:34 +0700

    buat contoh koneksi ke rest api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyApplication />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function MyApplication() {
+  const [myData, setMyData] = useState(null);
+
+  useEffect(() => {
+    async function ngambilDataPakeRestApi() {
+      // promise => janji (tidak bisa ditunaikan sekarang)
+      // result:
+      // A. janji ditepati (resolved dengan baik, resolved dengan tidak baik)
+      // B. janji tidak ditepati (rejected)
+      // lakukan sesuatu jika resolved dengan tidak baik.
+      // lakukan sesuatu jika resolved dengan baik.
+      try {
+        const respon = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=50",
+          {
+            method: "GET",
+          }
+        );
+        if (!respon.ok) {
+          throw "Ada error gan";
+        } else {
+          const myData = await respon.json();
+          setMyData(myData);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    ngambilDataPakeRestApi();
+  }, []);
+
+  if (myData === null) {
+    return <div>Masih mengambil data...</div>;
+  } else {
+    return <div>{JSON.stringify(myData)}</div>;
+  }
+}
